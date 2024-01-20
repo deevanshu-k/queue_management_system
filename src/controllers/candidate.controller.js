9e646ba1b9b1678c2c5ccd0e149fc273b3c62593
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2024-01-21 01:58:18 +0530

    ADD: Route POST /api/candidate + candidate.controller.addCandidateToQueue

--- src/controllers/candidate.controller.js
+++ src/controllers/candidate.controller.js
@@ -1,11 +1,37 @@
 const { celebrate, Joi } = require("celebrate");
+const uuidv4 = require("uuid").v4;
+const db = require("../models");
 
 module.exports.addCandidateToQueue = {
     validator: celebrate({
-        body: Joi.object(),
+        body: Joi.object().keys({
+            candidates: Joi.array()
+                .items(
+                    Joi.object()
+                        .keys({
+                            candidate_id: Joi.string().required(),
+                            name: Joi.string().required(),
+                        })
+                        .required()
+                )
+                .required(),
+        }),
     }),
     controller: async (req, res) => {
         try {
+            const queueId = req.auth.id;
+            let candidateData = [...req.body.candidates].map((d) => {
+                return {
+                    id: uuidv4(),
+                    queueId: queueId,
+                    ...d,
+                };
+            });
+            await db.candidate.bulkCreate(candidateData);
+
+            return res.status(200).json({
+                message: "Added successfully!",
+            });
         } catch (error) {
             console.log(error);
             return res.status(500).send({
